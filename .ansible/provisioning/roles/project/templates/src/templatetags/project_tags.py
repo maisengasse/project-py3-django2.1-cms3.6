f83d37f9fa905e9641c33ac46e8ee2d89df03769
armin%s64883.gridserver.com <armin%s64883.gridserver.com@28ae004f-017b-0410-b11a-bb4beccae6b6>
2019-01-17 17:22:53 +0000

    work on default
    git-svn-id: svn+ssh://maisengasse.at/home/64883/data/repos/maisengasse/projekte/boilerplates/ansible-py3.6-dj1.11-cms-3.5@23487 28ae004f-017b-0410-b11a-bb4beccae6b6

--- .ansible/provisioning/roles/project/templates/src/templatetags/project_tags.py
+++ .ansible/provisioning/roles/project/templates/src/templatetags/project_tags.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import math, datetime, re
+from bs4 import BeautifulSoup
 from django import template
+from filer.models import Image, File
 from django.conf import settings
 from django.utils.safestring import mark_safe
 from maisen.cmstools.utils import render_string
+from maisen.cmstools.filerpool.models import RemoteFilerImage
+from maisen.cmstools.templatetags.cmstools_tags import striptags2
 from maisen.cmstools.videotools import is_youtube_video, is_vimeo_video
 from maisen.cmstools.filerpool.templatetags.imagesize_tags import sizeurl
+from maisen.cmstools.utils import slugify
 
 register = template.Library()
 
@@ -26,9 +31,9 @@ def sourcecode(value, mobile=False):
 def richtext(value):
     if not value:
         return ""
-    return mark_safe(value)
 
-    # soup = BeautifulSoup(value)
+    soup = BeautifulSoup(value)
+
     # for table in soup.findAll('table'):
     #     #remove table attrs
     #     for attr in ('width', 'style',
@@ -37,28 +42,24 @@ def richtext(value):
     #         if table.get(attr):
     #             del(table[attr]);
 
-    #     for i, tr in enumerate(table.findAll('tr')):
-    #         # tr['class'] = "row%s %s" % (i+1, (i+1) % 2 and 'odd' or 'even')
-    #         tds = tr.findAll('td');
-    #         for j, td in enumerate(tds):
-    #             cw = 100.0/(len(tds))
-    #             td['style'] = "width:%.1f%%" % cw
-    #             # if 'tabelle-jahre' in table.get('class', ""):
-    #             #     del(td['style'])
-    #             # else:
-    #             # td['class'] = "col%s %s" % (j+1, (j+1) % 2 and 'odd' or 'even')
-    # return mark_safe(soup.renderContents())
+    # link titles
+    for a in soup.findAll('a'):
+        if not a.get('title'):
+            a['title'] = a.text
+
+    return mark_safe(soup.renderContents())
+
 
 
 @register.simple_tag(takes_context=True)
 def seo_description(context, default=""):
     desc = default
-    if context.get('page_description'):
-        desc = context['page_description']
+    if context.get('seo_description'):
+        desc = striptags2(context['seo_description'])
     else:
         tpl = "{{ '{%' }} load cms_tags{{ '%}' }}{{ '{%' }} page_attribute 'meta_description' {{ '%}' }}"
         d = render_string(tpl, context)
-        if d != u'None' or not d:
+        if d and d != u'None':
             desc = d
     return desc
 
@@ -66,18 +67,20 @@ def seo_description(context, default=""):
 @register.simple_tag(takes_context=True)
 def seo_title(context, suffix="", default=""):
     title = default
-    if context.get('page_title'):
-        title = context['page_title']
+    if context.get('seo_title'):
+        title = context['seo_title']
     else:
         tpl = "{{ '{%' }} load cms_tags{{ '%}' }}{{ '{%' }} page_attribute 'page_title' {{ '%}' }}"
         t = render_string(tpl, context)
-        if t != u'None' or not t:
+        if t and t != u'None':
             title = t
     return title + suffix
 
+
 @register.simple_tag(takes_context=True)
 def seo_image(context):
-    if context.get('page_image'):
-        image = context['page_image']
-        return settings.PORTAL_URL + sizeurl(image, 'fleximage')
-    return ""
+    if context.get('seo_image'):
+        image = context['seo_image']
+    else:
+        image = Image.objects.get(id=settings.DEFAULT_SEO_IMAGE)
+    return sizeurl(image, 'ogimage')
