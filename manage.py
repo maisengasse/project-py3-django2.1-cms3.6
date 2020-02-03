75dadffcde88dd0269529631ca6cb59538c011b8
Markus Digruber <markus@maisengasse.at>
2020-02-03 18:03:41 +0100

    fixes

--- manage.py
+++
@@ -1,9 +0,0 @@
-#!/home/vagrant/python-maisengasse/bin/python3.6
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maisengasse.settings.development")
-    os.environ['PYTHONIOENCODING'] = 'utf8'
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
