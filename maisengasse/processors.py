7c94ca2c7afa8eda37c9f39491679d19ca9a0127
Markus Digruber <markus@maisengasse.at>
2020-02-03 17:56:55 +0100

    Revert "initial setup"
    This reverts commit 0177ed156481ab93f4185caf8a1e238e8d77ca0f.

--- maisengasse/processors.py
+++
@@ -1,9 +0,0 @@
-# custom/processors.py
-from django.conf import settings
-from django.core.files.storage import default_storage
-from PIL import ImageFilter
-
-# def gauss(image, gauss=None, **kwargs):
-#     if gauss:
-#         image = image.filter(ImageFilter.GaussianBlur(radius=gauss))
-#     return image
--- maisengasse/translation.py
+++
@@ -1,6 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from cms.models import CMSPlugin
-
-class CMSPluginTO(TranslationOptions):
-    fields = ()
-translator.register(CMSPlugin, CMSPluginTO)
