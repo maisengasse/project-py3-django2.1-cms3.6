e5bf8103ffaf8c9fd577f15bdcdb55ccab1268cf
Markus Digruber <markus@maisengasse.at>
2020-02-21 13:36:08 +0100

    changes

--- .ansible/provisioning/roles/project/templates/src/settings/base.py
+++ .ansible/provisioning/roles/project/templates/src/settings/base.py
@@ -178,7 +178,7 @@ LANGUAGE_CODE = "de"
 TIME_ZONE = "Europe/Vienna"
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
